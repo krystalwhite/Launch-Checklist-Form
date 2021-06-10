091d2c02131d9f40c27baacc99895f8eb27b8c71
krystalwhite <krystalsnowhite@gmail.com>
2021-06-10 15:40:15 -0500

    Issues alert if fields are left blank.

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function() {
-   let form = document.getElementById("launchForm");
+   let form = document.querySelector("form");
+   let pilot = document.querySelector("input[name=pilotName]");
+   let copilot = document.querySelector("input[name=copilotName]");
+   let fuel = document.querySelector("input[name=fuelLevel]");
+   let cargo = document.querySelector("input[name=cargoMass]");
+
+   // let form = document.getElementById("launchForm");
    let launch = document.getElementById("launchStatus");
    let formSubmit = document.getElementById("formSubmit");
-   let pilot = document.getElementById("pilotStatus");
-   let copilot = document.getElementById("copilotStatus");
-   let fuel = document.getElementById("fuelStatus");
-   let cargo = document.getElementById("cargoStatus");
+   
+   
 
-   form.addEventListener("submit", function() {
-      submit.preventDefault();
-
+   form.addEventListener("submit", function(event) {
+      if (pilot.value === "" || copilot.value === "" || fuel.value === "" || cargo.value === "") {
+         alert("All fields are required to submit the form.");
+         event.preventDefault();
+      } 
    });
-
 });
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
