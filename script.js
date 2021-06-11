ecd8fbb2bb2353c3170b31a1eda5ab450cd9bc6e
krystalwhite <krystalsnowhite@gmail.com>
2021-06-11 13:55:23 -0500

    Added json; multiple attempts at input validator

--- script.js
+++ script.js
@@ -10,31 +10,41 @@ window.addEventListener("load", function() {
    let launch = document.getElementById("launchStatus");
    let formSubmit = document.getElementById("formSubmit");
    
-   
-   // the typeof for string and number are not catching
+   // the typeof for string and number are not catching -- both are logging as strings
    form.addEventListener("submit", function(event) {
       if (pilot.value === "" || copilot.value === "" || fuel.value === "" || cargo.value === "") {
          alert("All fields are required to submit the form.");
          event.preventDefault();
       } else if ((typeof(pilot.value) !== "string") || (typeof(copilot.value) !== "string")) {
+         console.log(`The pilot value is a ${typeof(pilot.value)}.`);
          alert("Please enter a name with letters only please.");
          event.preventDefault();
-      } else if ((typeof(fuel.value) != "number") || (typeof(cargo.value) != "number")) {
-         alert("Please enter a quantity with numbers only please.");
+      } else if (fuel.value < 0 || cargo.value < 0) {
+         alert("Please enter a positive quantity.");
          event.preventDefault();
       }
    });
 });
 
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+// This block of code shows how to format the HTML once you fetch some planetary JSON!
+
+fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+      let mission = document.getElementById("missionTarget");        
+      mission.innerHTML += `
+      <div id = "missionTarget">
+            <h2>Mission Destination</h2>
+                <ol> 
+                    <li>Name: ${json[3].name}</li>
+                    <li>Diameter: ${json[3].diameter}</li>
+                    <li>Star: ${json[3].star}</li>
+                    <li>Distance from Earth: ${json[3].distance}</li>
+                    <li>Number of Moons: ${json[3].moons}</li>
+                </ol>
+                </div>
+                <img class="missionTarget img" src="${json[3].image}">
+                </div>`;
+               
+        });
+});
