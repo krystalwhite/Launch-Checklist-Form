06e2512bb5a5d1e0750f2720e5d14d444cd771dc
krystalwhite <krystalsnowhite@gmail.com>
2021-06-14 16:17:11 -0500

    modified into long series of if/else if/else blocks

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// Write your JavaScript code here!
+
+/* TODO items:  
+1. determine how to make isNan work
+2. how to change text within the textbox
+
+ */
+
 window.addEventListener("load", function() {
-   // let form = document.querySelector("launchForm");
    let pilot = document.querySelector("input[name=pilotName]");
    let copilot = document.querySelector("input[name=copilotName]");
    let fuel = document.querySelector("input[name=fuelLevel]");
@@ -19,70 +24,61 @@ window.addEventListener("load", function() {
    // the typeof for string and number are not catching -- both are logging as strings
    form.addEventListener("submit", function(event) {
       event.preventDefault();
-      
 
-      if (pilot.value === "" || copilot.value === "" || fuel.value === "" || cargo.value === "") {
+      let pilotInputTest = Number(pilot.value);
+      let copilotInputTest = Number(copilot.value);
+      let fuelInput = Number(fuel.value);
+      let cargoInput = Number(cargo.value);
+
+      // NOTE: The "inputs are empty" code block tests correctly.
+      if (pilot.value === "" || copilot.value === "" || fuel.value === "" || cargo.value === "") { 
          alert("All fields are required to submit the form.");
-         event.preventDefault();
-      }  else if ((typeof(pilot.value) !== "string") || (typeof(copilot.value) !== "string")) {
-         console.log(`The pilot value is a ${typeof(pilot.value)}.`);
-         alert("Please enter a name with letters only please.");
-         event.preventDefault();
-      } else if (fuel.value < 0 || cargo.value < 0) {
+      }  
+       
+      // TODO: figure out how to make isNan work
+      // else if (isNan(pilotInputTest) || isNan(copilotInputTest)) {  //textbook and W3 disagree here
+      //    alert("Please enter a name with letters only please.");
+      //    console.log(`The pilot input is ${pilotInput}, the copilot input is ${copilotInput} and this if statement was triggered.`);
+      // }
+      
+      // else if (Number.isNan(fuelInput) || Number.isNan(cargoInput)) {
+      //    alert("Please enter a quantity please.");
+      // }
+
+      // NOTE: The "inputs are negative" code block tests correctly.
+      else if (fuel.value < 0 || cargo.value < 0) {  
          alert("Please enter a positive quantity.");
-         event.preventDefault();
       }
 
+      // NOTE: first three lines work; bottom line does not
+      else if (fuel.value < 10000) {
+         launch.style.color = "red";  // text of launchStatus color should be red
+         launch.innerHTML = "Shuttle not ready for launch";  // text of launchStatus should change to (`Shuttle not ready for launch`);
+         notReady.style.visibility = "visible";  // change faultyitems to visible
+         // fuelStatus.innerHTML(`There is not enough fuel for the journey.`); // change fuelStatus text
+         
+      }
+
+      // NOTE: first three lines work; bottom line does not
+      else if (cargo.value > 10000) {
+         launch.style.color = "red";  // text of launchStatus color should be red
+         launch.innerHTML = "Shuttle not ready for launch";  // text of launchStatus should change to (`Shuttle not ready for launch`);
+         notReady.style.visibility = "visible";  // change faultyitems to visible     
+         // cargoStatus.innerHTML(`There is too much mass for liftoff.`);  // change cargoStatus to state (`There is too much mass for liftoff.`);
+      }
+
+      // NOTE: first line won't work, but other two will
+      else {
+         // launch.innerHTML = "Shuttle is ready for launch";  // text of launchStatus should change to (`Shuttle is ready for launch`);
+         launch.style.color = "green"; //text of launchStatus color should be green
+         notReady.style.visibility = "visible";  // change faultyitems to visible
+      }
+
+
+
    });
-
-
-   document.addEventListener("click", function(event) {
-      let awaitingInfo = "";
-
-      pilotStatus.innerHTML += pilot.value;
-      copilotStatus.innerHTML += copilot.value;
-
-
-   });
-
-
-   if (fuel.value < 10000) {
-      // change fuelStatus to state (`There is not enough fuel for the journey.`);
-      // notReadyForLaunch;
-   }
-
-   if (cargo.value > 10000) {
-      // change fuelStatus to state (`There is not enough fuel for the journey.`);
-      // notReadyForLaunch;
-   }
-
-   function notReadyForLaunch() {
-      console.log(`testing out the notreadyforlaunch function`);
-      notReady.attribute = "visible";   // change faultyitems to visible
-      // launch.attribute 
-
-      // text of launchStatus should change to (`Shuttle not ready for launch`);
-      // text of launchStatus color should be red
-   }
-
-   // function readyForLaunch() {
-   //    // text of launchStatus color should be green
-   //    // text of launchStatus should change to (`Shuttle is ready for launch`);
-   // };
-
-
-
-
-   // pilotStatus.innerHTML(`Pilot ${pilot.value} is ready for launch.`);
-
-
-
-
 
 });
-
-
-notReadyForLaunch;
 
 
 // All this code works; disabling it right now during the construction phase.
